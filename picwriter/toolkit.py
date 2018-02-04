331af871cbe12d3e093207d1f5355a1822d4a7f5
Derek Kita <dkita@mit.edu>
2018-02-04 11:13:11 -0500

    test

--- picwriter/toolkit.py
+++ picwriter/toolkit.py
@@ -6,6 +6,8 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import gdspy
+
 class Cell:
     """
     Cell class for PICwriter toolkit that make it easier to manipulate for 
