67ed953fc4190e6218df14ef1e1830dd0ffa134a
Derek Kita <dkita@mit.edu>
2018-02-05 16:36:29 -0500

    fix

--- picwriter/tests/test_tookit.py
+++ picwriter/tests/test_tookit.py
@@ -7,10 +7,19 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from unittest import TestCase
 
-import picwriter.toolkit as tk
+import gdspy
+from picwriter.components.waveguide import Waveguide, WaveguideTemplate
 
 """ Test case below not necessary, but should serve as framework for later """
 class TestToolkit(TestCase):
 	def test_cell_creation(self):
-		c = tk.Cell("cell1")
+	    top = gdspy.Cell("top")
+	    wgt = WaveguideTemplate(bend_radius=50, resist='+', fab="LIFTOFF")
+
+	    wg1=Waveguide([(50,0), (250,0), (250,500), (500,500)], wgt)
+	    wg2=Waveguide([(0,0), (0,100), (-250, 100), (-250, -100)], wgt)
+
+	    top.add(wg1)
+	    top.add(wg2)
+
 		#self.assertTrue(isinstance(c.name, basestring))
