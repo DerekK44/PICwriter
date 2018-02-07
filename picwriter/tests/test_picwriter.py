f5e37c8e65eeb265a0e603225f181d50bf58cb17
Derek Kita <dkita@mit.edu>
2018-02-06 22:11:27 -0500

    reset testsuite

--- picwriter/tests/test_picwriter.py
+++ picwriter/tests/test_picwriter.py
@@ -21,9 +21,9 @@ class TestPICwriter(TestCase):
 		wg2=Waveguide([(0,0), (0,100), (-250, 100), (-250, -100)], wgt)
 		tk.add(top, wg1)
 		tk.add(top, wg2)
-		# print("Waveguide area = "+str(top.area()))
+		print("Waveguide area = "+str(top.area()))
 		self.assertTrue(len(top.elements)==2)
-		self.assertTrue(abs(top.area()-2828.31852764) <= 1e-6)
+		self.assertTrue(abs(top.area()-2828.31811879) <= 1e-6)
 
 	def test_taper_creation(self):
 		top = gdspy.Cell("t2")
@@ -34,9 +34,9 @@ class TestPICwriter(TestCase):
 		tp2 = Taper(wgt, 100.0, 0.0, **wg1.portlist["output"])
 		tk.add(top, tp1)
 		tk.add(top, tp2)
-		# print("Taper area = "+str(top.area()))
+		print("Taper area = "+str(top.area()))
 		self.assertTrue(len(top.elements)==3)
-		self.assertTrue(abs(top.area()-22141.5926399) <= 1e-6)
+		self.assertTrue(abs(top.area()-22141.5912852) <= 1e-6)
 
 	def test_grating_coupler_creation(self):
 		top = gdspy.Cell("t3")
@@ -47,15 +47,15 @@ class TestPICwriter(TestCase):
 		tk.add(top, gc1)
 		gc2 = GratingCouplerFocusing(wgt, focus_distance=20.0, width=20, length=50, period=1.0, dutycycle=0.7, **wg1.portlist["output"])
 		tk.add(top, gc2)
-		# print("Grating coupler area = "+str(top.area()))
+		print("Grating coupler area = "+str(top.area()))
 		self.assertTrue(len(top.elements)==3)
-		self.assertTrue(abs(top.area()-22562.0664901) <= 1e-6)
+		self.assertTrue(abs(top.area()-22562.0651354) <= 1e-6)
 
 	def test_spiral_creation(self):
 		top = gdspy.Cell("t4")
 		wgt = WaveguideTemplate(bend_radius=50, resist='-')
 		sp1 = Spiral(wgt, 1000.0, 1000.0, 10000.0)
 		tk.add(top, sp1)
-		# print("Spiral area = "+str(top.area()))
+		print("Spiral area = "+str(top.area()))
 		self.assertTrue(len(top.elements)==1)
-		self.assertTrue(abs(top.area()-20099.9998867) <= 1e-6)
+		self.assertTrue(abs(top.area()-20099.9984558) <= 1e-6)
