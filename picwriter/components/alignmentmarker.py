8ad066b5ca161b267bf96ba5d6b7533459877947
DerekK88 <dkita@mit.edu>
2019-07-01 17:18:54 -0700

    Port AlignmentMarker -> tk.PICcomponent

--- picwriter/components/alignmentmarker.py
+++ picwriter/components/alignmentmarker.py
@@ -5,7 +5,7 @@ import numpy as np
 import gdspy
 import picwriter.toolkit as tk
 
-class AlignmentCross(gdspy.Cell):
+class AlignmentCross(tk.PICcomponent):
     """ Cross Cell class (subclass of gdspy.Cell), used for alignment
 
         Args:
@@ -20,32 +20,49 @@ class AlignmentCross(gdspy.Cell):
 
     """
     def __init__(self, cross_length, cross_width, small_cross_width=None, center=(0,0), layer=1, datatype=0):
-        gdspy.Cell.__init__(self, tk.getCellName("AlignmentCross"))
+        tk.PICcomponent.__init__(self, "AlignmentCross")
 
         self.cross_length = cross_length
         self.cross_width = cross_width
         self.small_cross_width = cross_width/4.0 if small_cross_width==None else small_cross_width
         self.layer=layer
         self.datatype=datatype
-        self.center = center
+        
+        self.port = center
+        self.direction = 'EAST'
 
+        self.portlist = {}
         self.__build_cell()
+        self.__build_ports()
+        
+        """ Translate & rotate the ports corresponding to this specific component object
+        """
+        self._auto_transform_()
+        
+        """ The _hash_cell_ function makes sure that duplicate cells are not created.
+        Pass to it all the unique properties of this cell, which are used to check for duplicates.
+        Do *not* include properties like port, direction.  These are specific to Cell References only.
+        """
+        self._hash_cell_(cross_length, cross_width, small_cross_width, layer, datatype)
 
     def __build_cell(self):
         # Sequentially build all the geometric shapes, then add it to the Cell
-        x0,y0 = self.center
+        x0,y0 = (0,0)
 
         #Add big cross arms
-        self.add(gdspy.Rectangle((x0-self.cross_length, y0-self.cross_width/2.0), (x0-self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
-        self.add(gdspy.Rectangle((x0+self.cross_length, y0-self.cross_width/2.0), (x0+self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
-        self.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0-self.cross_length), (x0+self.cross_width/2.0, y0-self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
-        self.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0+self.cross_length), (x0+self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0-self.cross_length, y0-self.cross_width/2.0), (x0-self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0+self.cross_length, y0-self.cross_width/2.0), (x0+self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0-self.cross_length), (x0+self.cross_width/2.0, y0-self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0+self.cross_length), (x0+self.cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
 
         #Add little cross arms
-        self.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0-self.small_cross_width/2.0), (x0+self.cross_width/2.0, y0+self.small_cross_width/2.0), layer=self.layer, datatype=self.datatype))
-        self.add(gdspy.Rectangle((x0-self.small_cross_width/2.0, y0-self.cross_width/2.0), (x0+self.small_cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0-self.cross_width/2.0, y0-self.small_cross_width/2.0), (x0+self.cross_width/2.0, y0+self.small_cross_width/2.0), layer=self.layer, datatype=self.datatype))
+        self.cell.add(gdspy.Rectangle((x0-self.small_cross_width/2.0, y0-self.cross_width/2.0), (x0+self.small_cross_width/2.0, y0+self.cross_width/2.0), layer=self.layer, datatype=self.datatype))
 
-class AlignmentTarget(gdspy.Cell):
+    def __build_ports(self):
+        self.portlist["center"] = {'port':(0,0), 'direction':self.direction}
+        
+class AlignmentTarget(tk.PICcomponent):
     """ Standard Target Cell class (subclass of gdspy.Cell), used for alignment.  Set of concentric circles
 
         Args:
@@ -60,31 +77,50 @@ class AlignmentTarget(gdspy.Cell):
 
     """
     def __init__(self, diameter, ring_width, num_rings=10, center=(0,0), layer=1, datatype=0):
-        tk.getCellName("AlignmentTarget")
+        tk.PICcomponent.__init__(self, "AlignmentTarget")
 
         self.diameter = diameter
         self.ring_width = ring_width
         self.num_rings = num_rings
-        self.center = center
         self.layer=layer
         self.datatype=datatype
+        
+        self.port = center
+        self.direction = 'EAST'
+        self.portlist = {}
 
         self.build_cell()
+        self.__build_ports()
+        
+        """ Translate & rotate the ports corresponding to this specific component object
+        """
+        self._auto_transform_()
+        
+        """ The _hash_cell_ function makes sure that duplicate cells are not created.
+        Pass to it all the unique properties of this cell, which are used to check for duplicates.
+        Do *not* include properties like port, direction.  These are specific to Cell References only.
+        """
+        self._hash_cell_(self.diameter, self.ring_width, self.num_rings, self.layer, self.datatype)
+        print(self.cell_hash)
+        print("num_rings = "+str(self.num_rings))
 
     def build_cell(self):
         # Sequentially build all the geometric shapes, then add it to the Cell
-        x0,y0 = self.center
+        x0,y0 = (0,0)
         spacing = self.diameter/(4.0*self.num_rings)
         for i in range(self.num_rings):
-            self.add(gdspy.Round((x0,y0), 2*(i+1)*spacing, 2*(i+1)*spacing-self.ring_width, layer=self.layer, datatype=self.datatype, number_of_points=0.1))
+            self.cell.add(gdspy.Round((x0,y0), 2*(i+1)*spacing, 2*(i+1)*spacing-self.ring_width, layer=self.layer, datatype=self.datatype, number_of_points=0.1))
 
+    def __build_ports(self):
+        self.portlist["center"] = {'port':(0,0), 'direction':self.direction}
 
 if __name__ == "__main__":
     from . import *
     top = gdspy.Cell("top")
-    mark1 = AlignmentCross(500, 1, center=(0,0))
-    mark2 = AlignmentCross(500, 1, center=(100,1000))
-#    mark1 = AlignmentTarget(200, 3, num_rings=10)
+#    mark1 = AlignmentCross(500, 1, center=(0,0))
+#    mark2 = AlignmentCross(500, 1, center=(100,1000))
+    mark1 = AlignmentTarget(200, 3, num_rings=10)
+    mark2 = AlignmentTarget(200, 3, num_rings=11, center=(200,300))
     tk.add(top, mark1)
     tk.add(top, mark2)
 
