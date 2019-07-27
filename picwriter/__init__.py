7f2e67532da1ad52aa1dad8f92823d3ed18be773
DerekK88 <dkita@mit.edu>
2019-07-27 18:21:16 -0400

    update copyright

--- picwriter/__init__.py
+++ picwriter/__init__.py
@@ -6,7 +6,7 @@ from .toolkit import get_keys, get_trace_length
 
 """
 ================================================================================================================================
-Copyright 2018 Derek M. Kita
+Copyright 2019 Derek M. Kita
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge,
